4f31346b47ff5bdb77a803a3d28779e41b8c8c18
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2024-05-28 22:38:59 +0530

    Time: 12 ms (6.30%) | Memory: 9.4 MB (12.01%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.cpp
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-    int equalSubstring(string s, string t, int maxCost) {
-        int n = s.size();
-        vector<int> v(n + 1, 0);
-
-        for (int i = 0; i < n; i++) {
-            v[i + 1] = abs(s[i] - t[i]) + v[i];
+    int equalSubstring(string s, string t, int maxCost) {int n=s.size();
+        vector<int> v(n+1,0);
+        for(int i=0;i<n;i++){
+            v[i+1]=abs(s[i]-t[i])+v[i];
         }
-
-        int low = 0, high = n, mid, prevMid;
-        while (low <= high) {
-            mid = low + (high - low) / 2;
-            bool found = false;
-
-            for (int i = 0; i <= n - mid; i++) {
-                if (v[i + mid] - v[i] <= maxCost) {
-                    found = true;
-                    break;
+        int low=0,high=n,mid=0,prev=-1;
+        while(low<=high && prev!=mid){
+           prev=mid; mid=(high+low)/2;int f=0;
+            for(int i=0;i<(n+1-mid);i++){
+                if((v[i+mid]-v[i]) <=maxCost){
+                    f=1;break;
                 }
             }
-
-            if (found) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+            if(f)low=(mid+1);
+            else high=(mid-1);
+            
         }
-
-        return high;
+        return low-1;
     }
 };
