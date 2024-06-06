feabd7988436adee2b375031b9ec0f0bb240bd86
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2024-06-06 12:12:33 +0530

    Time: 109 ms (83.36%) | Memory: 55.7 MB (22.19%) - LeetSync

---
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPossibleDivide(vector<int>& hand, int groupSize) {
+                unordered_map<int,int> mp;int ans=1;
+        for(int i:hand)mp[i]++;
+        vector<int> v;for(auto i:mp)v.push_back(i.first);sort(v.begin(),v.end());
+        for(int i:v){if(!ans)break;
+            if(mp[i]){int temp=mp[i];
+                for(int j=i;j<(i+groupSize);j++){
+                    if(mp[j]<temp){ans=0;break;}
+                    mp[j]-=temp;
+                }
+            }
+        }return ans;
+    }
+};
