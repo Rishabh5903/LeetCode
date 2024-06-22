9db8795ab5580b92d8df97c3d1a23a379211b574
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2024-06-22 23:06:58 +0530

    Time: 98 ms (47.67%) | Memory: 74.1 MB (25.59%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        vector<int> ind;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]%2)ind.push_back(i);
+        vector<int> x;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] % 2 != 0){
+                x.push_back(i);
+            }
         }
-        int ans=0;
-        // if(v.size()<k)return 0;
-        int l=0;int a=1,b=1;
-        for(int r=k-1;r<ind.size();r++){
-            if(l>0)a=ind[l]-ind[l-1];else a=ind[l]+1;
-            if(r<ind.size()-1)b=ind[r+1]-ind[r];else b=nums.size()-ind[r];
-            ans+=a*b;l++;
-        }return ans;
+        
+        if (x.size() < k) return 0; 
+
+        int count = 0;
+        int a = 1, b = 1;
+        for(int j = 0; j < x.size() - k + 1; j++){
+            if(j == 0){
+                a = x[j] + 1;
+            } else {
+                a = x[j] - x[j - 1];
+            }
+            if(j + k == x.size()){
+                b = nums.size() - x[j + k - 1];
+            } else {
+                b = x[j + k] - x[j + k - 1];
+            }
+            count += a * b;
+        }
+        return count;
     }
 };
