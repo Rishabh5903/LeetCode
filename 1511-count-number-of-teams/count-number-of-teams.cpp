0f3c7f60ac3a837dbe5987563bf11ee6c1d5be69
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2024-07-29 18:56:50 +0530

    Time: 31 ms (67.12%) | Memory: 12.3 MB (30.14%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.cpp
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
-        vector<pair<int,int>> v;
-        for (int i=0;i<rating.size();i++){
-            v.push_back({rating[i],i});
-        }sort(v.begin(),v.end());
-        int ans=0;
-        for(int i=0;i<v.size();i++){
-            int l1=0,r1=0,l2=0,r2=0;
-            for(int j=0;j<i;j++){
-                if(v[j].second<v[i].second)l1++;
+        vector<pair<int, int>> v;
+        for (int i = 0; i < rating.size(); i++) {
+            v.push_back({rating[i], i});
+        }
+        sort(v.begin(), v.end());
+        
+        int ans = 0;
+        for (int i = 0; i < v.size(); i++) {
+            int l1 = 0, r1 = 0, l2 = 0, r2 = 0;
+            for (int j = 0; j < i; j++) {
+                if (v[j].second < v[i].second) l1++;
                 else r2++;
             }
-            for(int j=i+1;j<v.size();j++){
-                if(v[j].second>v[i].second)r1++;
+            for (int j = i + 1; j < v.size(); j++) {
+                if (v[j].second > v[i].second) r1++;
                 else l2++;
-            }ans+=l1*r1+l2*r2;
-        }return ans;
+            }
+            ans += l1 * r1 + l2 * r2;
+        }
+        return ans;
     }
 };
