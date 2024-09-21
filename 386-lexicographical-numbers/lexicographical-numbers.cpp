5302f44cf1e1f78c5f58b34fbea2b52bd567bfdf
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2024-09-21 20:07:13 +0530

    Time: 4 ms (88.45%) | Memory: 12.6 MB (86.28%) - LeetSync

---
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        int curr=1;
+        vector<int> ans;
+        while(curr){
+            while(curr%10<=9){
+            while(curr<=n){
+                ans.push_back(curr);
+                curr*=10;
+                }
+                curr/=10;
+            while(curr%10==9)curr/=10;
+            if(curr==0)break;
+            curr++;
+            }
+            
+        }return ans;
+    }
+};
