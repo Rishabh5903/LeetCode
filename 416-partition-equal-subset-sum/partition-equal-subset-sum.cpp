496668da163a66a66eacadd150751ac975b851fe
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2024-12-05 19:33:45 +0530

    Time: 299 ms (20.65%) | Memory: 57.1 MB (50.01%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n=nums.size();
+        int tot=accumulate(nums.begin(),nums.end(),0);
+        if(tot%2)return false;
+        // tot/=2;
+        bool dp[201][200001];
+
+        memset(dp,false,sizeof(dp));
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=1;j<=tot;j++){
+                // if(i==n-1)if(nums[i]==j)dp[i][j]=true;
+                // else{
+                if(nums[i]<=j)dp[i][j]=(dp[i][j] || dp[i+1][j-nums[i]] || dp[i+1][j]);
+                else dp[i][j]=(dp[i][j] || dp[i+1][j]);
+                // }
+                
+            }
+        }
+        return dp[0][tot/2];
+    }
+};
