0031f17d4f3a84fd0fedaac2934bf19b4ca683f2
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-03-13 02:04:19 +0530

    Time: 2 ms (52.25%) | Memory: 10.5 MB (76.62%) - LeetSync

--- 224-basic-calculator/basic-calculator.cpp
+++ 224-basic-calculator/basic-calculator.cpp
@@ -1,19 +1,22 @@
 class Solution {
-public:
-    int calculate(string s) {int n=s.size();
-        stack<char> st;int neg=0;
-        string num="0";int ans=0;char prev='+';
-        for(int i=0;i<n;i++){char c=s[i];
-            if(!isdigit(c)){ans+=((neg%2)?-1:1)*((prev=='+')?1:-1)*stoi(num);num="0";}
-            if(c=='+'){prev='+';
-                if(s[i+1]=='(' || (s[i+1]==' ' && s[i+2]=='(')){st.push(c);}
-            }
-            else if(c=='-'){prev='-';if(s[i+1]=='(' || (s[i+1]==' ' && s[i+2]=='(')){st.push(c);neg++;prev='+';}}
-            if(c==')' && st.size()){if(st.top()=='-')neg--;st.pop();}
-            if(isdigit(c))num+=c;
-
-        }
-        ans+=((neg%2)?-1:1)*((prev=='+')?1:-1)*stoi(num);
-        return ans;
-    }
+ public:
+  int calculate(string s) {
+    int ans = 0;
+    int num = 0;
+    int sign = 1;
+    stack<int> stack{{sign}};  
+    for (const char c : s)
+      if (isdigit(c))
+        num = num * 10 + (c - '0');
+      else if (c == '(')
+        stack.push(sign);
+      else if (c == ')')
+        stack.pop();
+      else if (c == '+' || c == '-') {
+        ans += sign * num;
+        sign = (c == '+' ? 1 : -1) * stack.top();
+        num = 0;
+      }
+    return ans + sign * num;
+  }
 };
