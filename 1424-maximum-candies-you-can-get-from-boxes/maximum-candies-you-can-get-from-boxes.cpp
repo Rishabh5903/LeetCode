2208103189b675362d8a65f972b6b6d711ab3cc1
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-06-03 22:22:45 +0530

    Time: 39 ms (8.17%) | Memory: 46.2 MB (10.58%) - LeetSync

---
+++ 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+        int n=status.size();
+        vector<int> vis(n,0);
+        queue<int> q;set<int> key,s;
+        for (int i:initialBoxes){q.push(i);s.insert(i);}
+        int ans=0;
+        while(q.size()){
+            int curr=q.front();
+            if((key.find(curr)!=key.end()) || (status[curr]) ){
+                ans+=candies[curr];q.pop();
+                for(int i:keys[curr])key.insert(i);
+                for(int i:containedBoxes[curr])if(s.find(i)==s.end()){q.push(i);s.insert(i);}
+                // open[curr]=1;
+                // s.erase(curr);
+            }
+            else if(vis[curr]==2){q.pop();}
+            else {
+                q.pop();q.push(curr);
+                vis[curr]+=1;}
+        }
+        return ans;
+    }
+};
