1734a4fd14fc75bd9c8307379ab68e9833a5cbfa
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-06-04 13:04:25 +0530

    Time: 210 ms (5.97%) | Memory: 10.2 MB (91.04%) - LeetSync

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -5,9 +5,10 @@ public:
         int m=word.size();
         // char c='a';
         // for(char ch:word)c=max(c,ch);
-        string ans="";string temp="";
+        string ans="";string temp="";char prev='a';
         for(int i=0;i<m;i++){
-            temp="";
+            prev=max(prev,word[i]);
+            if(word[i]<prev)continue;temp="";
             for(int j=i;j<min(m,i+m-n+1);j++)temp+=word[j];
             ans=max(ans,temp);
         }
