a9fe79687cccdf8a30d8faee28be33b90649f638
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-06-07 22:42:40 +0530

    Time: 163 ms (32.14%) | Memory: 26.4 MB (61.69%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     string clearStars(string s) {
         priority_queue<pair<char,int>,vector<pair<char,int>>, greater<pair<char,int>>> pq;
-        vector<int> ind;
+        vector<bool> keep(s.size(),true);
         for(int i=0;i<s.size();i++){
             if(s[i]!='*'){
                 pq.push({s[i],-i});
             }
             else{
-                ind.push_back(i);
-                ind.push_back(-pq.top().second);pq.pop();
+                keep[i]=0;
+                keep[-pq.top().second]=0;pq.pop();
             }
         }
-        sort(ind.begin(),ind.end());
+        // sort(ind.begin(),ind.end());
         string ans="";int j=0;
         for(int i=0;i<s.size();i++){
-            if(j<ind.size() && i==ind[j]){j++;continue;}
-            else ans+=s[i];
+            if(keep[i])ans+=s[i];
         }
         return ans;
     }
