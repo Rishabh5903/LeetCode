cbee1996196373a74da3ebebff809b002c98008f
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-06-13 01:00:50 +0530

    Time: 132 ms (37.13%) | Memory: 34.3 MB (27.69%) - LeetSync

---
+++ 87-scramble-string/scramble-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    unordered_map<string,bool> mp;
+    bool isScramble(string s1, string s2) {
+        int n=s1.size();if(s2.size()!=n)return false;if(s1==s2)return true;if(s1.size()==1)return false;
+        // bool b1=false,b2=false;
+        string curr=s1+","+s2;
+        if(mp.find(curr)!=mp.end())return mp[curr];
+        for(int i=1;i<n;i++){
+            if(isScramble(s1.substr(0,i),s2.substr(0,i)) && isScramble(s1.substr(i),s2.substr(i)))return mp[curr]=true;
+            if(isScramble(s1.substr(0,i),s2.substr(n-i)) && isScramble(s1.substr(i),s2.substr(0,n-i)))return mp[curr]=true;
+            
+        }
+        return mp[curr]=false;
+    }
+};
