b37eb69169f555f71da032619ec4bd6622e28e8a
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-06-13 01:02:03 +0530

    Time: 14 ms (78.05%) | Memory: 16.1 MB (78.43%) - LeetSync

--- 87-scramble-string/scramble-string.cpp
+++ 87-scramble-string/scramble-string.cpp
@@ -6,6 +6,10 @@ public:
         // bool b1=false,b2=false;
         string curr=s1+","+s2;
         if(mp.find(curr)!=mp.end())return mp[curr];
+        string temp1 = s1, temp2 = s2;
+        sort(temp1.begin(), temp1.end());
+        sort(temp2.begin(), temp2.end());
+        if (temp1 != temp2) return mp[curr] = false;
         for(int i=1;i<n;i++){
             if(isScramble(s1.substr(0,i),s2.substr(0,i)) && isScramble(s1.substr(i),s2.substr(i)))return mp[curr]=true;
             if(isScramble(s1.substr(0,i),s2.substr(n-i)) && isScramble(s1.substr(i),s2.substr(0,n-i)))return mp[curr]=true;
