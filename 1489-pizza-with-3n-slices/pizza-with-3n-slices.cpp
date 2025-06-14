545cdf38134d8a45dfb83dea0f318f687bc9ad43
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-06-14 12:50:10 +0530

    Time: 11 ms (61.54%) | Memory: 18.4 MB (41.83%) - LeetSync

--- 1489-pizza-with-3n-slices/pizza-with-3n-slices.cpp
+++ 1489-pizza-with-3n-slices/pizza-with-3n-slices.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
-    int dp[501][170][2];
-    int solve(vector<int>& slices,int i,int rem,int flag){
-        if(rem==0)return dp[i][rem][flag]=0;
-        if(i>=slices.size() || ((i==slices.size()-1) && flag))return INT_MIN;
-        if(dp[i][rem][flag]!=-1)return dp[i][rem][flag];
-        return dp[i][rem][flag]=max(solve(slices,i+1,rem,(i==0 ? 0 : flag)),slices[i]+solve(slices,i+2,rem-1,(i==0 ? 1 : flag)));
-    }
     int maxSizeSlices(vector<int>& slices) {
-        int n=slices.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(slices,0,n/3,0);
+        int n = (int)slices.size() / 3;
+        auto l1 = vector<int>(slices.begin(), slices.end()-1);
+        auto l2 = vector<int>(slices.begin()+1, slices.end());
+        return max(linear(l1, n), linear(l2, n));
+    }
+    
+private:
+    int linear(vector<int>& slices, int n) {
+        vector<vector<int>> eat((int)slices.size()+2, vector<int>(n+1, INT_MIN));
+        int res = INT_MIN;
+        for (int i=0; i<eat.size(); ++i) eat[i][0] = 0;
+        for (int i=2; i<eat.size(); ++i) {
+            for (int j=1; j<n+1; ++j)
+                eat[i][j] = max(eat[i-1][j], eat[i-2][j-1] + slices[i-2]);
+            res = max(eat[i][n], res);
+        }
+        return res;
     }
 };
