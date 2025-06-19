c2d8ce5b7fed8a0a39a6283a30ce1915c218b62e
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-06-19 19:31:51 +0530

    Time: 1 ms (93.50%) | Memory: 44.5 MB (64.13%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,12 +1,9 @@
 class Solution {
     public void solve(List<List<Integer>> ans,int [] nums,List<Integer> temp){
-        if(temp.size()==nums.length){ans.add(temp);return;}
+        if(temp.size()==nums.length){List<Integer> temp1 = new ArrayList<>(temp);ans.add(temp1);return;}
         for(int i=0;i<nums.length;i++){
             if(temp.contains(nums[i])==false){
-                
-                temp.add(nums[i]);
-                List<Integer> temp1 = new ArrayList<>(temp);
-                solve(ans,nums,temp1);
+                temp.add(nums[i]);solve(ans,nums,temp);
                 temp.remove(temp.size()-1);
             }
         }
