76fd991e76fd0a9462d7505eab8755d78e940eee
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-06-24 00:30:46 +0530

    Time: 2303 ms (11.40%) | Memory: 553.7 MB (8.77%) - LeetSync

--- 2202-sum-of-k-mirror-numbers/sum-of-k-mirror-numbers.cpp
+++ 2202-sum-of-k-mirror-numbers/sum-of-k-mirror-numbers.cpp
@@ -1,41 +1,47 @@
+
 class Solution {
 public:
-    bool isPalindrome(const string &s) {
-        int i = 0, j = s.size() - 1;
-        while (i < j) if (s[i++] != s[j--]) return false;
+    long long createPalindrome(long long num, bool odd) {
+        long long x = num;
+        if (odd) x /= 10;
+        while (x > 0) {
+            num = num * 10 + x % 10;
+            x /= 10;
+        }
+        return num;
+    }
+
+    bool isPalindrome(long long num, int base) {
+        vector<int> digits;
+        while (num > 0) {
+            digits.push_back(num % base);
+            num /= base;
+        }
+        int i = 0, j = digits.size() - 1;
+        while (i < j) {
+            if (digits[i++] != digits[j--]) return false;
+        }
         return true;
     }
 
-    bool isKMirror(long long x, int k) {
-        long long t = x;
-        string s;
-        while (t > 0) {
-            s.push_back('0' + (t % k));
-            t /= k;
-        }
-        reverse(s.begin(), s.end());
-        return isPalindrome(s);
-    }
-
     long long kMirror(int k, int n) {
-        vector<long long> result;
-        for (int len = 1; result.size() < n; ++len) {
-            int half = (len + 1) / 2;
-            long long start = pow(10, half - 1);
-            long long end = pow(10, half) - 1;
-            if (half == 1) start = 1;
-            for (long long prefix = start; prefix <= end && result.size() < n; ++prefix) {
-                long long pal = prefix;
-                long long tmp = prefix / (len % 2 ? 10 : 1);
-                while (tmp > 0) {
-                    pal = pal * 10 + (tmp % 10);
-                    tmp /= 10;
+        long long sum = 0;
+        for (long long len = 1; n > 0; len *= 10) {
+            for (long long i = len; n > 0 && i < len * 10; i++) {
+                long long p = createPalindrome(i, true);
+                if (isPalindrome(p, k)) {
+                    sum += p;
+                    n--;
                 }
-                if (isKMirror(pal, k)) result.push_back(pal);
+            }
+            for (long long i = len; n > 0 && i < len * 10; i++) {
+                long long p = createPalindrome(i, false);
+                if (isPalindrome(p, k)) {
+                    sum += p;
+                    n--;
+                }
             }
         }
-        long long sum = 0;
-        for (int i = 0; i < n; ++i) sum += result[i];
         return sum;
     }
 };
