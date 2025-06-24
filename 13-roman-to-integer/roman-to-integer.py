8fa3d64d1d2e0837a9bfa1fb63eae4f17d4edac0
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-06-24 13:09:08 +0530

    Time: 12 ms (8.93%) | Memory: 17.8 MB (64.44%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,28 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        map={}
+        map['I']=1
+        map['V']=5
+        map['X']=10
+        map['L']=50
+        map['C']=100
+        map['D']=500
+        map['M']=1000
+        map['IV']=4
+        map['IX']=9
+        map['XL']=40
+        map['XC']=90
+        map['CD']=400
+        map['CM']=900
+        ans = 0
+        i=0
+        while(i<len(s)):
+            if(i<len(s)-1 and (s[i:i+2] in map)):
+                ans+= map[s[i:i+2]]
+                i+= 2
+            else:
+                ans+= map[s[i]]
+                i+=1
+        return(ans)
+        
+        
