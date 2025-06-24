5c7abd49c0731ba37cc5339ee9b1a02971674121
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-06-24 13:14:12 +0530

    Time: 3 ms (79.69%) | Memory: 18 MB (20.53%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,28 +1,25 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        map={}
-        map['I']=1
-        map['V']=5
-        map['X']=10
-        map['L']=50
-        map['C']=100
-        map['D']=500
-        map['M']=1000
-        map['IV']=4
-        map['IX']=9
-        map['XL']=40
-        map['XC']=90
-        map['CD']=400
-        map['CM']=900
-        ans = 0
-        i=0
-        while(i<len(s)):
-            if(i<len(s)-1 and (s[i]+s[i+1] in map)):
-                ans+= map[s[i]+s[i+1]]
-                i+= 2
+
+        val = {
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        }
+        
+        value=0
+        max_so_far=0
+        for i in range(len(s)-1,-1,-1):
+            el = s[i]
+            digit = val[el]
+            max_so_far = max(digit,max_so_far)
+            if digit<max_so_far:
+                value-=digit
             else:
-                ans+= map[s[i]]
-                i+=1
-        return(ans)
-        
-        
+                value+=digit
+                
+        return value
