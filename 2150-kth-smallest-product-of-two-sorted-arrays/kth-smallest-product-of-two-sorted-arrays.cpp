c6d0e2f577a95c6a1d21465f44321dd7d3a154a3
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-06-25 23:42:20 +0530

    Time: 560 ms (68.28%) | Memory: 98.1 MB (70.09%) - LeetSync

---
+++ 2150-kth-smallest-product-of-two-sorted-arrays/kth-smallest-product-of-two-sorted-arrays.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+bool chk(long long mid, vector<int>& nums1, vector<int>& nums2, long long k){
+    long long cnt=0;
+    for(int i:nums1){
+        if(i==0){
+            if(mid>=0)cnt+=nums2.size();
+            continue;
+        }
+        long long target = mid/i;
+        if((mid%i !=0) && mid<0)if(i<0)target++;else target--;
+        long long ind;
+        if(i<0){ind=lower_bound(nums2.begin(),nums2.end(),target)-nums2.begin();cnt+=nums2.size()-ind;}
+        else if(i>0){ind=upper_bound(nums2.begin(),nums2.end(),target)-nums2.begin();cnt+=ind;}
+    }
+    return cnt>=k;
+}
+    long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, long long k) {
+        long long lo=-1e10,hi=1e10;long long mid;
+        while(lo<=hi){
+            mid=lo+(hi-lo)/2;
+            if(chk(mid,nums1,nums2,k))hi=mid-1;else lo=mid+1;
+        }
+        return hi+1;
+    }
+};
