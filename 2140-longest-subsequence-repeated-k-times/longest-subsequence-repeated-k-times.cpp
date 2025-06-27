9bd2226c0f48f3b58f9e2938b7b5c41d65452adf
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-06-27 22:26:00 +0530

    Time: 1225 ms (6.52%) | Memory: 11.5 MB (77.54%) - LeetSync

---
+++ 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool chk(string& s,string& temp,int& k){
+        int cnt=0;int j=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]==temp[j])j++;
+            if(j==temp.size()){cnt++;j=0;}
+            if(cnt==k)return true;
+        }
+        return cnt>=k;
+    }
+    void solve(string& ans,string& s,string& temp,int& k,vector<int>& fr,vector<int>& curr){
+        if(chk(s,temp,k) && ans.size()<temp.size())ans=temp;
+        if(temp.size()==8)return;
+        for(int i=25;i>=0;i--){
+            if(curr[i]<(fr[i]/k)){
+                temp+=('a'+i);curr[i]++;solve(ans,s,temp,k,fr,curr);
+                temp=temp.substr(0,temp.size()-1);curr[i]--;
+            }
+        }
+    }
+    string longestSubsequenceRepeatedK(string s, int k) {
+        string ans="";
+        vector<int> fr(26,0);
+        for(char c:s)fr[c-'a']++;
+        vector<int> curr(26,0);
+        string temp="";
+        solve(ans,s,temp,k,fr,curr);
+        return ans;
+    }
+};
