2b2578c9cc81f2c301a8d82b47751abbe5486efa
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-07-08 23:20:26 +0530

    Time: 307 ms (9.17%) | Memory: 173.7 MB (7.41%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -10,8 +10,8 @@ public:
         for(auto i:events)start.push_back(i[0]);
         for(int i=0;i<n+1;i++)dp[i][0]=0;
         for(int j=1;j<=k;j++)dp[n][j]=0;
-        for(int j=1;j<=k;j++)dp[n-1][j]=events[n-1][2];
-        for(int i=n-2;i>=0;i--){
+        // for(int j=1;j<=k;j++)dp[n-1][j]=events[n-1][2];
+        for(int i=n-1;i>=0;i--){
             for(int j=1;j<=k;j++){
                 int ind=upper_bound(start.begin(),start.end(),events[i][1])-start.begin();
                 dp[i][j]=max(dp[i][j],max(dp[ind][j-1]+events[i][2],dp[i+1][j]));
