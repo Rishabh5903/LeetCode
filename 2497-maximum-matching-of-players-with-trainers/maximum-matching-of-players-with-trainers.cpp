4fb8dca2efc2037a5643109cf4e644379e5e409c
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-07-14 00:21:47 +0530

    Time: 27 ms (77.62%) | Memory: 80.3 MB (24.63%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.cpp
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.cpp
@@ -6,17 +6,9 @@ public:
         int ans=0;
         int i=0,j=0;
         while(i<players.size() && j<trainers.size()){
-            if(players[i]<=trainers[j]){i++;j++;ans++;}
-            else j++;
+            if(players[i]<=trainers[j]){i++;ans++;}
+            j++;
         }
         return ans;
     }
 };
-#define LC_HACK
-#ifdef LC_HACK
-const auto __ = []() {
-  struct ___ { static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; } };
-  std::atexit(&___::_);
-  return 0;
-}();
-#endif
