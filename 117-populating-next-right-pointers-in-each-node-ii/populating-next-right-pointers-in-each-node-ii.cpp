94c78c509f5fbaa2f73da9f1139fb31963ee8704
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-10-04 11:53:35 +0530

    Time: 8 ms (67.77%) | Memory: 19 MB (12.58%) - LeetSync

--- 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.cpp
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.cpp
@@ -22,17 +22,20 @@ public:
         if(!root)return root;
         queue<Node*> q;
         q.push(root);
-        vector<Node*> v;
+        Node* prev= new Node(); Node* curr=new Node();
         while(q.size()){
             int sz=q.size();
-            v.clear();
-            for(int i=0;i<sz;i++){
-                v.push_back(q.front());
-                if(q.front()->left)q.push(q.front()->left);
-                if(q.front()->right)q.push(q.front()->right);
-                q.pop();
+            prev=q.front();
+            if(prev->left)q.push(prev->left);
+            if(prev->right)q.push(prev->right);
+            q.pop();
+            for(int i=1;i<sz;i++){
+                curr=q.front();
+                if(curr->left)q.push(curr->left);
+                if(curr->right)q.push(curr->right);
+                q.pop();prev->next=curr;
+                prev=curr;
             }
-            for(int i=0;i<sz-1;i++)v[i]->next=v[i+1];
 
         }
         return root;
