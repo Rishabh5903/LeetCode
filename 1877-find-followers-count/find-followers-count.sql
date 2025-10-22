0548de5f824e9657eb1f6defc2c8c8560735763c
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-10-22 21:36:37 +0530

    Time: 594 ms (22.65%) | Memory: 0B (100.00%) - LeetSync

--- 1877-find-followers-count/find-followers-count.sql
+++ 1877-find-followers-count/find-followers-count.sql
@@ -1,2 +1,4 @@
-# Write your MySQL query statement below
-select user_id, count(follower_id) followers_count from Followers group by user_id order by user_id asc
+-- Write your PostgreSQL query statement below
+SELECT user_id, COUNT(distinct follower_id) AS followers_count
+FROM Followers
+GROUP BY user_id;
