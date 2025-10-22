c89a4d4f5050c29c862ca68513fb5100c460230f
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2025-10-22 20:32:12 +0530

    Time: 1498 ms (27.58%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -5,7 +5,7 @@ where s.name not in
     (
         select s.name
         from SalesPerson s
-            left join Orders o on o.sales_id = s.sales_id
+            right join Orders o on o.sales_id = s.sales_id
             left join Company c on o.com_id = c.com_id
         where c.name = 'Red'
     )
