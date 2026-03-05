b2f1ff8dc66f0373cbe7fc9c0a5b132976a2f4d0
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2026-03-06 01:02:10 +0530

    Time: 783 ms (15.94%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,16 +1,16 @@
 # Write your MySQL query statement below
-select distinct p.product_id,
-       case when f.new_price is not null then f.new_price else 10 end as price
-from products p
+select p.product_id, COALESCE(f.new_price,10) as price
+from (select distinct product_id from products) p
 left join (
-    select p1.product_id, p1.new_price, p1.change_date
+    select p1.product_id, p1.new_price
     from products p1
     join (
         select product_id, max(change_date) as change_date
         from products
         where change_date <= '2019-08-16'
         group by product_id
-    ) x
-    on p1.product_id = x.product_id and p1.change_date = x.change_date
+    ) t
+    on p1.product_id = t.product_id
+    and p1.change_date = t.change_date
 ) f
 on p.product_id = f.product_id;
