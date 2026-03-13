73aa87d6874b4a34c7270bdda9b9ad567212c46a
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2026-03-13 19:42:51 +0530

    Time: 11 ms (54.86%) | Memory: 12.8 MB (52.27%) - LeetSync

--- 836-race-car/race-car.cpp
+++ 836-race-car/race-car.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
     int racecar(int target) {
-        vector<int> dp(target + 1, 1e9);
-        dp[0] = 0;
-
-        for(int i = 1; i <= target; i++){
-            int n = log2(i) + 1;
-            int curr = (1 << n) - 1;
-
-            if(curr == i){
-                dp[i] = n;
-                continue;
-            }
-
-            dp[i] = min(dp[i], n + 1 + dp[curr - i]);
-
-            for(int prev = 0; prev < n - 1; prev++){
-                int back = (1 << prev) - 1;
-                int pos = (1 << (n - 1)) - 1;
-                int rem = i - (pos - back);
-                dp[i] = min(dp[i], (n - 1) + 1 + prev + 1 + dp[rem]);
+        // 1. Initialize double ended queue as 0 moves, 0 position, +1 velocity
+        queue<vector<int>> q;
+        q.push({0, 0, 1});
+        while (!q.empty()) {
+            
+            vector<int> item = q.front();
+            q.pop();
+            
+            // (moves) moves, (pos) position, (vel) velocity)
+            int moves = item[0];
+            int pos = item[1];
+            int vel = item[2];
+            
+            if (pos == target) return moves;
+            
+            // Avoid integer overflow - stop whenever we've passed twice the magnitude of target.
+            if (abs(pos) > 2 * target) continue;
+            
+            // 2. Always consider moving the car in the direction it is already going
+            q.push({moves + 1, pos + vel, 2 * vel});
+            
+            // 3. Only consider changing the direction of the car if one of the following conditions is true
+            //    i.  The car is driving away from the target.
+            //    ii. The car will pass the target in the next move.  
+            if ((pos + vel > target && vel > 0) || (pos + vel < target && vel < 0)) {
+                q.push({moves + 1, pos, (vel > 0) ? -1 : 1});
             }
         }
-
-        return dp[target];
+        
+        return 0;
     }
 };
