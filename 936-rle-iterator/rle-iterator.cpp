ad4444c8acec223cf032ae96d37099069c792286
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2026-03-13 20:22:18 +0530

    Time: 2 ms (11.24%) | Memory: 14.6 MB (11.80%) - LeetSync

---
+++ 936-rle-iterator/rle-iterator.cpp
@@ -0,0 +1,35 @@
+class RLEIterator {
+public:
+    vector<int> v;
+    int ind=0, exhausted=0;
+    RLEIterator(vector<int>& encoding) {
+        v=encoding;
+    }
+    
+    int next(int n) {
+        int ans=-1;
+        while(ind<v.size()-1 && v[ind]<=n && n){
+            n-=v[ind];
+            v[ind]=0;
+            ans = v[ind+1];
+            // if(n==0)break;
+            ind+=2;
+            
+
+        }
+        if(ind != v.size() && v[ind]>=n && n){
+            v[ind]-=n;
+            n=0;
+            ans=v[ind+1];
+        }
+
+        if(n==0)return ans;
+        return -1;
+    }
+};
+
+/**
+ * Your RLEIterator object will be instantiated and called as such:
+ * RLEIterator* obj = new RLEIterator(encoding);
+ * int param_1 = obj->next(n);
+ */
