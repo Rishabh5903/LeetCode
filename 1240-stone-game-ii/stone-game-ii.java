07ff2d1388314a6ac5036a3debe63dc15407b53b
Rishabh Kumar Jaiswal <97736490+Rishabh5903@users.noreply.github.com>
2026-05-24 12:30:06 +0530

    Time: 9 ms (45.29%) | Memory: 44.1 MB (84.55%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.java
+++ 1240-stone-game-ii/stone-game-ii.java
@@ -7,9 +7,15 @@ class Solution {
             psum[i] = psum[i-1] + piles[i-1];
         }
         for(int i=n;i>=1;i--){
+            int total = psum[n] - psum[i - 1];
+            int rem = n - i + 1;
             for(int m=n;m>=1;m--){
+                if (2 * m >= rem) {
+                    dp[i][m] = total;
+                    continue;
+                }
                 for(int x=1;x<=Math.min(2*m, n - i +1);x++){
-                    dp[i][m] = Math.max(dp[i][m], -psum[i-1] + psum[n] - dp[i+x][Math.max(m,x)]);
+                    dp[i][m] = Math.max(dp[i][m], total - dp[i+x][Math.max(m,x)]);
                 }
             }
         }
